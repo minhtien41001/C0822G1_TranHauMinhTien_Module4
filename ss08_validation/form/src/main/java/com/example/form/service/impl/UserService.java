8668e737849830de6403e6913f846b5659b26aba
minhtien41001 <tranminhtien41001@gmail.com>
2022-12-26 08:11:20 +0700

    update

--- ss08_validation/form/src/main/java/com/example/form/service/impl/UserService.java
+++ ss08_validation/form/src/main/java/com/example/form/service/impl/UserService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService implements IUserService {
     @Autowired
-    IUserRepository iUserRepository;
+    private IUserRepository iUserRepository;
 
     @Override
     public Page<User> findAll(Pageable pageable) {
